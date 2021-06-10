66d2494d1a4616c2f3b816353b7d09dec00e51e7
André Rodrigues <andre.rodrigues98@outlook.com>
2021-06-09 21:22:05 -0300

    add middlewares and providers

---
+++ src/intl/messages/en-US.ts
@@ -0,0 +1,16 @@
+const messages = {
+  errors: {
+    INTERNAL_ERROR_SERVER: 'Internal Error Server',
+    INTERNAL_ERROR_SERVER_MESSAGE: 'Something is wrong!',
+  },
+  keys: {
+    logs: {
+      START_LOG: 'app:essential:start',
+    },
+  },
+  logs: {
+    INITIAL_LOG: '🚀️ Server started on port 3333!',
+  },
+};
+
+export default messages;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
+import 'express-async-errors';
 import express from 'express';
+import helmet from 'helmet';
 
 import routes from './routes';
+import handleErrors from './middlewares/handle-errors';
+import DebugLogProvider from './providers/DebugLogProvider';
+import messages from './intl/messages/en-US';
 
 const app = express();
 
+app.use(helmet());
+
 app.use('/', routes);
 
+app.use(handleErrors);
+
 app.listen(3333, () => {
-  console.log('🚀️ Server started on port 3333!');
+  const debugLogProvider = new DebugLogProvider(messages.keys.logs.START_LOG);
+  debugLogProvider.log({ message: messages.logs.INITIAL_LOG });
 });
